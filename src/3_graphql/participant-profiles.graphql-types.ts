fde895523a2e8e1447dc92e4532a9e2fd6ccd65b
Kyusung4698 <>
2022-03-10 10:14:20 +0000

    Preview

---
+++ src/3_graphql/participant-profiles.graphql-types.ts
@@ -0,0 +1,46 @@
+import { Client } from '../common/league';
+
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type Scalars = {
+    ID: string;
+    String: string;
+    Boolean: boolean;
+    Int: number;
+    Float: number;
+};
+
+export type GetParticipantProfilesInput = {
+    platformId: Scalars['String'];
+    queueId: Scalars['String'];
+    participants: Array<InputMaybe<ParticipantInput>>;
+};
+
+export type ParticipantInput = {
+    summonerName: Scalars['String'];
+    role?: InputMaybe<Client.Role>;
+    championId?: InputMaybe<Client.ChampionId>;
+};
+
+export type Rank = {
+    __typename?: 'Rank';
+    tier: Client.Tier;
+    division: Client.Division;
+};
+
+export type Profile = {
+    __typename?: 'Profile';
+    kda?: Maybe<Scalars['Float']>;
+    gamesPlayed: Scalars['Int'];
+    winrate: Scalars['Float'];
+    rank?: Maybe<Rank>;
+};
+
+export type ParticipantProfile = {
+    __typename?: 'ParticipantProfile';
+    summonerName: Scalars['String'];
+    queueProfile?: Maybe<Profile>;
+    championProfile?: Maybe<Profile>;
+    roleProfile?: Maybe<Profile>;
+};
